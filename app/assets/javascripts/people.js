951b54d610025ecd0bab31042cfa07ef656fc943
beefoo <brian@youaremyjoy.org>
2015-07-27 17:15:03 -0400

    First pass at person identification ui

--- app/assets/javascripts/people.js
+++ app/assets/javascripts/people.js
@@ -28,35 +28,124 @@
       this.loadData();
     };
 
-    People.prototype.addRegion = function(active){
-
-      var $region = $('<div class="region"></div>');
-
+    People.prototype.activateRegion = function(id){
       $('.region').removeClass('active');
-      if (active) $region.addClass('active');
+      $('.region[data-id="'+id+'"]').addClass('active');
+    };
+
+    People.prototype.addRegion = function(id, active){
+
+      var _this = this,
+          $region = $('<div class="region" data-id="'+id+'"><div class="region-form">'+
+            '<div class="selections"><label>Select a name:</label><div class="selection-list"></div><label>Or:</label></div>'+
+            '<form class="input"><input value="" class="input-name" placeholder="Enter a name" /></form>'+
+            '<div class="links"><a href="#delete" class="delete-link">Delete this region</a></div>'+
+          '</div></div>');
+
+      // add region to ui
       this.$asset.append($region);
 
+      // activate and update button text
+      if (active) {
+        this.activateRegion(id);
+      }
+      this.buttonText("I'm Finished");
+
+      // make it draggable
       $region.draggable({
-        start: function(e){ e.stopPropagation(); },
+        start: function(e){
+          e.stopPropagation();
+          _this.activateRegion(id);
+          _this.formHide();
+        },
         drag: function(e){ e.stopPropagation(); },
         stop: function(e){
           e.stopPropagation();
           var values = _this.getRegionData();
           values.id = $(this).attr('data-id');
-          _this.submit(values, false);
+          _this.submit();
+          _this.formShow();
         }
+
+      // make it resizable
       }).resizable({
-        start: function(e){ e.stopPropagation(); },
+        start: function(e){
+          e.stopPropagation();
+          _this.activateRegion(id);
+          _this.formHide();
+        },
         resize: function(e){ e.stopPropagation(); },
         stop: function(e){
           e.stopPropagation();
           var values = _this.getRegionData();
           values.id = $(this).attr('data-id');
-          _this.submit(values, false);
+          _this.submit();
+          _this.formShow();
         }
       });
 
+      // click on region, make active
+      $region.on('click', function(e){
+        e.preventDefault();
+        _this.activateRegion(id);
+        _this.formShow();
+      });
+
+      // delete region link
+      $region.find('.delete-link').on('click', function(e){
+        e.preventDefault();
+        e.stopPropagation();
+        _this.deleteRegion();
+      });
+
+      // submit input form
+      var $inputForm = $region.find('form.input').first();
+      $inputForm.on('submit', function(e){
+        e.preventDefault();
+        $region.find('.selection-list button').removeClass('active');
+        _this.submit(true);
+      });
+
+      this.loadFieldOptions();
+
       return $region;
+    };
+
+    People.prototype.buttonText = function(text){
+      $('.button-next').text(text);
+    };
+
+    People.prototype.deleteRegion = function(){
+      var $region = $('.region.active').first();
+
+      $region.remove();
+    };
+
+    People.prototype.fixFormPosition = function(){
+      var $region = $('.region.active').first(),
+          regionData = this.getRegionData(),
+          $form = $region.find('.region-form').first(),
+          offset = parseFloat($region.css('border-width')),
+          positions = [
+            {position: 'top', value: regionData.y, styles: {left: 0, top: -$form.outerHeight()-offset, right: 'auto', bottom: 'auto'}},
+            {position: 'left', value: regionData.x, styles: {left: -$form.outerWidth()-offset, top: 0, right: 'auto', bottom: 'auto'}},
+            {position: 'right', value: 100-regionData.w-regionData.x, styles: {right: -$form.outerWidth()-offset, top: 0, left: 'auto', bottom: 'auto'}},
+            {position: 'bottom', value: 100-regionData.h-regionData.y, styles: {left: 0, bottom: -$form.outerHeight()-offset, right: 'auto', top: 'auto'}}
+          ],
+          position = _.max(positions, function(p){ return p.value });
+
+      $form.css(position.styles);
+    };
+
+    People.prototype.formHide = function(){
+      $('.region.active .region-form').removeClass('active');
+    };
+
+    People.prototype.formShow = function(){
+      var $form = $('.region.active .region-form');
+      this.fixFormPosition();
+      $form.addClass('active');
+      $form.find('.input-name').first().focus();
     };
 
     People.prototype.getRegionData = function(){
@@ -95,6 +184,36 @@
       });
     };
 
+    People.prototype.loadFieldOptions = function(){
+      var _this = this,
+          $region = $('.region.active').first(),
+          $container = $region.find('.selections').first(),
+          $list = $container.find('.selection-list').first(),
+          names = ['Bob Smith', 'Jane Doe', 'Barbara Smith'];
+
+      _.each(names, function(name){
+        $list.append('<button>'+name+'</button>');
+      });
+
+      if (names.length > 0) {
+        $container.addClass('active');
+        this.message("Enter/select the person's name if known or drag a box around another face");
+
+        // select button form
+        $list.find('button').on('click', function(e){
+          e.preventDefault();
+          $list.find('button').removeClass('active');
+          $(this).addClass('active');
+          $region.find('.input-name').val($(this).text());
+          _this.submit(true);
+        });
+
+      } else {
+        _this.message("Enter the person's name if known or drag a box around another face");
+      }
+
+    };
+
     People.prototype.loadListeners = function(){
       var _this = this;
 
@@ -109,7 +228,7 @@
 
       // Starting to drag rectangle, remember start values
       $asset.hammer().bind('panstart', function(e) {
-        // e.stopPropagation();
+        e.stopPropagation();
         assetX = $asset.offset().left;
         assetY = $asset.offset().top;
         assetW = $asset.width();
@@ -117,12 +236,13 @@
         var eventX = e.gesture.center.x,
             eventY = e.gesture.center.y,
             offsetX = eventX - assetX,
-            offsetY = eventY - assetY;
+            offsetY = eventY - assetY,
+            id = _.uniqueId('region_');
         startX = offsetX;
         startY = offsetY;
 
         // add region
-        _this.addRegion(true);
+        _this.addRegion(id, true);
       });
 
       // Dragging rectangle, update rectangle
@@ -148,8 +268,7 @@
 
       // Stopped dragging rectangle: submit coordinates/dimensions
       $asset.hammer().bind('panend', function(e) {
-        var id = _.uniqueId('field_'),
-            regionValue = _this.getRegionData(),
+        var regionValue = _this.getRegionData(),
             $region = $('.region.active').first();
 
         // update rectangle css
@@ -158,10 +277,9 @@
           height: regionValue.h+'%',
           left: regionValue.x+'%',
           top: regionValue.y+'%'
-        }).attr('data-id', id);
+        });
 
-        // position the field type selector
-        // _this.updateFieldTypesPosition(regionValue);
+        _this.formShow();
       });
 
     };
@@ -212,12 +330,33 @@
           'background-image': 'url("'+item.img_url+'")'
         });
         _this.message("Select or drag a box around a person's face");
+        _this.buttonText("Skip");
       };
       temp_img.src = item.img_url;
     };
 
-    People.prototype.submit = function(field, animate){
-      this.options.debug && console.log('Submitting: '+field);
+    People.prototype.submit = function(animate){
+      var data = this.getRegionData(),
+          $region = $('.region.active').first(),
+          id = $region.attr('data-id'),
+          value = $region.find('.input-name').first().val();
+
+      data.id = id;
+      data.value = value;
+      this.options.debug && console.log('Submitting: '+data);
+
+      if (animate) {
+        setTimeout(function(){
+          $region.find('.region-form').first().animate({
+            'opacity': 0
+          }, 500, function(){
+            $region.removeClass('active');
+            $(this).css({
+              'opacity': 1
+            });
+          });
+        }, 500);
+      }
     };
 
     return People;
